835ccb35e31db9054c55d89bb00d88f321a6c238
Gerson Aponte <3xor18@gmail.com>
2020-05-24 14:00:05 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './app/navigations/Navigation'
+import { YellowBox } from 'react-native'
+import { decode, encode } from "base-64";
+
+YellowBox.ignoreWarnings(["Setting a timer"])
+if (!global.btoa) global.btoa = encode;
+if (!global.atob) global.atob = decode;
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
